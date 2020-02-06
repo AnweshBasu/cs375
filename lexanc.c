907927fadeafbb43e39dc39b59dacd8761385369
AnweshBasu <anweshbasu1998@gmail.com>
2020-02-05 21:29:54 -0600

    Update lexanc.c

--- lexanc.c
+++ lexanc.c
@@ -253,10 +253,10 @@ TOKEN number (TOKEN tok)
 	//printf("intVal = %d\n", intVal);
 	  if ( num > INT_MAX ) {
 	      printf("start int error = %d    exponent = %d\n", num,exponent);
-	      if (exponent == 0) 
-	      {
-	      	exponent = 1;
-	      }
+	      //if (exponent == 0) 
+	      //{
+	      //	exponent = 1;
+	      //}
 	      exponent --;
 	      intError = 1;
 	    } else {
@@ -265,10 +265,10 @@ TOKEN number (TOKEN tok)
 	    }
         }
 
-    if ( num > INT_MAX ) {
-    exponent --;
-    intError = 1;
-  } 
+    //if ( num > INT_MAX ) {
+    //exponent --;
+    //intError = 1;
+  //} 
   printf("final num = %d\n", num);
   if ((c = peekchar()) != EOF &&  c == '.') {
     if ((d = peek2char()) != EOF && CHARCLASS[d] == NUMERIC) {
