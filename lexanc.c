0a2a503f4b24b9957e877470e5f37d3dda0b6cc8
AnweshBasu <anweshbasu1998@gmail.com>
2020-02-05 18:34:23 -0600

    Update lexanc.c

--- lexanc.c
+++ lexanc.c
@@ -174,27 +174,23 @@ TOKEN special (TOKEN tok)
   while ( (c = peekchar()) != EOF
         && (size <= 3) && (CHARCLASS[c] == SPECIAL)) 
   {
-	printf("\nhere\n");
     special[size] = getchar();
     size += 1;
     if(d = peekchar() != EOF) {
       if ((c == ":" && d == "=") || (c == "<" && (d == ">" || d == "=")) || 
           (c == ">" && d == "=") || (c == "." && d == ".")) 
       {
-	      printf("double\n");
         special[size] = getchar();
         size += 1;
       }
     }
   }
-	printf("exit size = %d ... val = %c\n",size, special[0]);
   special[size] = '\0';
 
   for (count = 0; count <= 12 ; count++)
   {
     if (strcmp(special, operators[count]) == 0)
     {
-	printf("found operator\n");
       tok->tokentype = OPERATOR;
       tok->whichval = count+1;
       return tok;
